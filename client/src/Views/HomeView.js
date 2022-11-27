af822a3d8f7f2797aba964373a32a23ff1836901
Vasques <nuno.a.vasques@outlook.com>
2022-11-27 04:57:00 +0000

    first commit

--- client/src/Views/HomeView.js
+++ client/src/Views/HomeView.js
@@ -6,10 +6,14 @@ import useFetch from "./useFetch";
 
 const Homeview = () => {
     const { data: prodsAux, isPending } = useFetch('http://localhost:8000/getProds');
+    const [prods, setProds] = useState(prodsAux);
+
+    /*
     const [filter, setFilter] = useState("All");
     const [prods, setProds] = useState(prodsAux);
     useEffect(() => {
         let result = prodsAux;
+        console.log("Entrei Agora");
         switch (filter) {
             case "All":
                 result = prodsAux;
@@ -25,14 +29,15 @@ const Homeview = () => {
         setProds(result)
         setProds(result)
 
-    }, [filter, isPending]);
-    console.log(prods);
+    }, [filter, isPending]); */
+
     return (
 
         <div className='body'>
+            {console.log(prods)}
             <div className='containerGeral'>
-                {prods != null &&
-                    prods.map(data => {
+                {prodsAux != null &&
+                    prodsAux.map(data => {
                         return (
                             <div className='produtoWrapper'>
                                 <div className='produtoCard'>
@@ -48,9 +53,6 @@ const Homeview = () => {
                         )
                     })
                 }
-
-
-
             </div>
         </div>
     );
